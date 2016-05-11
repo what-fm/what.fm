e07a9dc4ccf637c669d5420aac221a609fcacc88
Jude Taylor <me@jude.bio>
2016-05-11 13:21:10 -0700

    introduce flake8

--- default.nix
+++ default.nix
@@ -3,15 +3,25 @@
 with stdenv.lib;
 
 let basePythonPackages = pkgs.python34Packages;
-    myPythonPackages = basePythonPackages.override (a: {
-      self = myPythonPackages;
+
+    localOverrides = pythonPackages: {
+      what.fm = pythonPackages.what.fm.override (attrs: {
+        buildInputs = optional inNixShell basePythonPackages.flake8
+          ++ attrs.buildInputs;
+      });
+    };
+
+    localPythonPackages = basePythonPackages.override (a: {
+      self = localPythonPackages;
     })
     // (scopedImport {
-      self = myPythonPackages;
+      self = localPythonPackages;
       super = basePythonPackages;
       inherit pkgs;
       inherit (pkgs) fetchurl;
     } ./python-packages.nix)
     // { inherit (basePythonPackages) pip; };
+
+    myPythonPackages = localPythonPackages // localOverrides localPythonPackages;
 
 in myPythonPackages.what.fm
